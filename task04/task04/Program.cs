b8e808bfe2c60a6f53a027f63e8ce9db9e9d92ab
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-10-01 20:45:54 +0500

    Задание 4

---
+++ task04/task04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите действительное число:");
+
+            var x = double.Parse(Console.ReadLine());
+            var y = F(x);
+
+            Console.WriteLine("y = " + y);
+        }
+
+        static double F(double x)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Sqrt(Math.Pow(x, 2) + 1 / Math.Pow(x, 2)));
+        }
+    }
+}
