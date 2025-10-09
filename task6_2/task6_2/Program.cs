8e161004a63afb048cc97dcf3fbd72ef206c50fd
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-10-09 23:37:05 +0500

    Задание 6_2 (Вариант 14)

---
+++ task6_2/task6_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дано слово кинематограф");
+            string input = "кинематограф";
+            Console.WriteLine("Первое слово: " + firstConcat(input));
+            Console.WriteLine("Второе слово: " + secondConcat(input));
+        }
+
+        static string firstConcat(string word) 
+        {
+            string first = word.Substring(8);
+            string second = word.Substring(7, 1);
+            string third = word.Substring(4, 2);
+            string forth = word.Substring(2, 1);
+
+            var result = first + second + third + forth;
+            return(result);
+        }
+
+        static string secondConcat(string word) 
+        { 
+            string first = new string ((word.Substring(3, 2)).Reverse().ToArray());
+            //string first = word.Substring(4, 1);
+            //string second = word.Substring(3, 1);
+            string third = word.Substring(6, 1);
+            string forth = word.Substring(9, 1);
+            string fifth = word.Substring(7, 1);
+
+            //var result = first + second + third + forth + fifth;
+            var result = first + third + forth + fifth;
+            return(result);
+        }
+    }
+}
