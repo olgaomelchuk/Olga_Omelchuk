5911c3b60c2b0419be21b4996c2c8a4a4f3d3032
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-10-29 18:36:07 +0500

    Задание 9 (Вариант 14)

---
+++ task09/task09/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение аргумента:");
+
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"f({x:F2}) = {F(x):F2}");
+        }
+
+        static double F(double x)
+        {
+            if (x < -3)
+                return 0;
+            else if (x <= -1)
+                return 1;
+            else
+                return 2;
+        }
+    }
+}
