bf7b57f7a40aef0f30b38d485be91970e298b635
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-11-17 21:42:11 +0500

    Задание 10_1

---
+++ task10_1/task10_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число больше нуля");
+
+            int n;
+
+            if(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода!");
+                return;
+            }
+
+            if(n <= 0)
+            {
+                Console.WriteLine("Число должно быть больше нуля!");
+                return;
+            }
+
+            double sum = 0;
+
+            for (int a = 1; a <= n; a++)
+                sum += Math.Sqrt(a);
+
+            Console.WriteLine($"Сумма квадратных корней чисел от 1 до {n} равна {sum}");
+        }
+    }
+}
