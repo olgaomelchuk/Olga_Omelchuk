ecfae42f299ef14d2bb1635ddda62e132e0f11f3
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-11-17 23:56:49 +0500

    Задание 10_3

---
+++ task10_3/task10_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n - количество целых чисел:");
+
+            int n;
+
+            if (!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Ошибка ввода!");
+                return;
+            }
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Количество не может быть отрицательным или равняться нулю!");
+                return;
+            }
+
+            Console.WriteLine("Введите целые числа:");
+
+            int kMax;
+
+            if (!int.TryParse(Console.ReadLine(), out kMax))
+            {
+                Console.WriteLine("Введите целое число!");
+                return;
+            }
+
+            for (int a = 2; a <= n; a++)
+            {
+                int number;
+
+                if (!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.WriteLine("Ошибка ввода!");
+                    return;
+                }
+
+                if (number > kMax)
+                    kMax = number;
+            }
+
+            Console.WriteLine($"Наибольшее число: {kMax}");
+        }
+    }
+}
