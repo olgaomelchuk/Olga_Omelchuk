121f29c715155436b39369a5c23ec8f753896c1c
Ольга Омельчук <olgaomelchuk@bk.ru>
2025-12-21 03:27:00 +0500

    Задание 10_6

---
+++ task10_6/task10_6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueDigitsTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Трехзначные числа с разными цифрами в возрастающем порядке:");
+            Console.WriteLine();
+
+            for (int i = 100; i <= 999; i++)
+            {
+                int numb = i;
+
+                int d3 = numb % 10;          // единицы
+                int d2 = (numb / 10) % 10;   // десятки
+                int d1 = numb / 100;         // сотни
+
+                if (d1 != d2 && d1 != d3 && d2 != d3)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
